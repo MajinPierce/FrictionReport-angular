02075791273d54146b53a141bdd2cbb5fbdf196c
Pierce Beckett <42756176+MajinPierce@users.noreply.github.com>
2023-05-10 21:08:37 -0500

    basic structure (#2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MapComponent } from './pages/map/map.component';
+import { SidebarComponent } from './pages/dashboard/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    MapComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
