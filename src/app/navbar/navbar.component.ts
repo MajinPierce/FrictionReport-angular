1ad458c7518efc0d998fc5ff4b37e0e2378ad475
PierceBeckett <pierceabeckett@gmail.com>
2023-05-10 21:05:07 -0500

    basic structure

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'friction-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+
+}
