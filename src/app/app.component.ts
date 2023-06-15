5f81edd8133439b7f5cca943a997eec4b8bb1e36
PierceBeckett <pierceabeckett@gmail.com>
2023-06-15 16:22:58 -0500

    weather forecast icons and formatting, closes 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,47 @@ export class AppComponent {
     this.matIconRegistry.addSvgIcon(
       "open_in_new",
       this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/open_in_new_300.svg")
-    )
+    );
+    this.matIconRegistry.addSvgIcon(
+      "help",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/icons/help_300.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "sun",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/weather-icons/sun_300.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "moon",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/weather-icons/moon_300.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "partly_cloudy_day",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/weather-icons/partly_cloudy_day_300.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "partly_cloudy_night",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/weather-icons/partly_cloudy_night_300.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "cloudy",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/weather-icons/cloudy_300.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "rain",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/weather-icons/rain_300.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "thunderstorm",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/weather-icons/thunderstorm_300.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "snow",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/weather-icons/snow_300.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "foggy",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/weather-icons/foggy_300.svg")
+    );
   }
 
 }
