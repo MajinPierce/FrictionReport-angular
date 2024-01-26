5c512d3c1b38b2810f016bf254767b273e8d9aed
Pierce Beckett <pierceabeckett@gmail.com>
2024-01-26 15:36:58 -0500

    github and email links, sidebar title link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   environmentName: "prod",
   apiUrl: "https://api.friction.report/",
-  siteUrl: "https://friction.report/"
+  siteUrl: "https://www.friction.report/"
 }
